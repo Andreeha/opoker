82515b8e8a6a444ea7ed562e83e355a2969c6ace
Andreeha <48863136+Andreeha@users.noreply.github.com>
2025-04-26 23:24:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ SRC="./src/"
 BUILD="./build/"
 INCLUDE="./include/"
 LIB="./lib/"
-CC="~/opt/android/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android23-clang"
 
 gcc -o ${BUILD}main ${SRC}main.c -I${INCLUDE} -L${LIB} -lraylib -lm
